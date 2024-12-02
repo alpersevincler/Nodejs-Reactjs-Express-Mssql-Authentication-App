c0243b353313785909be63dcc0d41fc74bab0098
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-12-02 17:50:08 +0300

    Update server.js
    database connection, port definition
    
    Update Register.jsx main.jsx App.jsx
    
    form design, bootstrap install, remove redundant code

--- server/server.js
+++ server/server.js
@@ -4,11 +4,36 @@ import mssql from 'mssql'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
+const config = {
+    user: 'CodingWithAlper',
+    password: '1234',
+    server: 'LAPTOP-B5A8-PMD',
+    database: 'SQL-Tutorial',
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: false,
+        enableArithAbort: true,
+        encrypt: true,
+        instancename: 'SQLEXPRESS'
+    },
+    port: 1433
+    // ,"driver": "msnodesqlv8",
+};
+
+const db = mssql.connect(config);
+
+
+app.listen(8081, () => {
+    console.log("Running on port 8081");
+})
 
 /* 
 const config = {
