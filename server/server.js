b679e780ebcd98023f184a62432665fcf7dc440b
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-12-02 13:39:40 +0300

    Update server.js
    test codes in comment line

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// package.json dosyasındaki "type": "module" ifadesi sayesinde bu şekilde import edebiliyoruz
+// package.json dosyasındaki "type": "module" ifadesi sayesinde bu şekilde import edebiliyoruz. Diğer şekilde require()'ı kullanmamız gerekirdi
 import express from 'express';
 import mssql from 'mssql'
 import cors from 'cors'
@@ -9,3 +9,41 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+/* 
+const config = {
+    user: 'CodingWithAlper',
+    password: '1234',
+    server: 'LAPTOP-B5A8-PMD',
+    database: 'SQL-Tutorial',
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: false,
+        enableArithAbort: true,
+        encrypt: true,
+        instancename: 'SQLEXPRESS'
+    },
+    port: 1433
+    // ,"driver": "msnodesqlv8",
+};
+
+const getDB = async() => {
+    try {
+        let pool = await mssql.connect(config);
+
+        let queryClick = `SELECT * from EmployeeDemographics`;
+
+        let dataDB = await pool.request().query(queryClick);
+
+        console.log("dataDB = ", dataDB.recordsets);
+
+        return dataDB;
+    }
+    catch(error) {
+        console.log("No = ", error);
+    }
+    
+}
+
+getDB();
+*/
