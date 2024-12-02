b192aff685b97e0c6e649db6f9b9d6154ccc0f72
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-12-02 18:08:10 +0300

    Update App.js import BrowserRouter Routes Route

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
