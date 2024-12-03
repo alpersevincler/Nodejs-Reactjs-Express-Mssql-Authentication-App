09779847cd8ea0ed55874ffe5ff389980200866f
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2024-12-03 11:26:58 +0300

    Create Login.jsx Home.jsx and Update Register.jsx main.jsx App.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Register from './Register'
+import Login from './Login'
+
 
 function App() {
   
@@ -8,6 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   )
--- frontEnd/src/Login.jsx
+++ frontEnd/src/Login.jsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Register() {
-
-    const [values, setValues] = useState({name: '', email: '', password: ''});
-
+function Login() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='bg-white p-3 rounded w-25'>
             <h2>Sign-Up</h2>
             <form action="">
-                <div className='mb-3'>
-                    <label htmlFor="name"><strong>Name</strong></label>
-                    <input type="text" placeholder='Enter Name' name='name' className='form-control rounded-0' />
-                </div>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>E-mail</strong></label>
                     <input type="email" placeholder='Enter E-mail' name='email' className='form-control rounded-0' />
@@ -22,13 +15,13 @@ function Register() {
                     <label htmlFor="password"><strong>Password</strong></label>
                     <input type="password" placeholder='Enter Password' name='password' className='form-control rounded-0' />
                 </div>
-                <button type='submit' className='btn btn-success w-100 rounded-0'>Sign up</button>
+                <button type='submit' className='btn btn-success w-100 rounded-0'>Log in</button>
                 <p>You are agree to aour terms and policies</p>
-                <Link to="/login" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
+                <Link to="/register" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
             </form>
         </div>
     </div>
   )
 }
 
-export default Register
+export default Login
